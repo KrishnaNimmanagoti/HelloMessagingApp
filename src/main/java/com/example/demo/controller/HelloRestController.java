4cbe2229ab4964f5a56a6d2fef64e3643daa00f0
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-22 18:22:32 +0530

    Used PUT request method and passed first as path variable name last name as QueryParameter

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,5 +40,12 @@ public class HelloRestController {
 	@PostMapping("/post")
 	public String sayHello(@RequestBody User user) {
 		return "Hello" + user.getFirstName() + " "  + user.getLastName() + "!";
+	}
+	
+//	curl -X PUT localhost:8080/hello/put/Krishna/?lastName=Nimmanagoti -w "\n"
+	@PutMapping("/put/{firstName}")
+	public String sayHello(@PathVariable String firstName,
+						   @RequestParam(value = "lastName") String lastName) {
+		return "Hello " + firstName + " " + lastName + "!";
 	}
 }
