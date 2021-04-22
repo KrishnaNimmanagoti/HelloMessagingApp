b115887334e7a624a560dfbb5a5bd21807d59f77
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-04-22 17:22:33 +0530

    Used GET request method and passed name as path variable

--- src/main/java/com/example/demo/controller/HelloRestController.java
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloRestController {
 	
+//	curl localhost:8080/hello -w "\n"
 	@RequestMapping(value = {"", "/", "/home"})
 	public String sayHello() {
 		return "Hello From Bridgelabz!!!";
@@ -20,4 +23,9 @@ public class HelloRestController {
 		return "Hello" + name + "!";
 	}
 	
+//	curl localhost:8080/hello/param/Krishna -w "\n"
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+		return "Hello" + name + "!";
+	}
 }
